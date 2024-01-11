e7a1d87965b1728538d94c5f3b3c9f53baa92f40
teller2016 <teller75@naver.com>
2024-01-11 18:14:31 +0900

    refactor: define types

--- src/composables/use/use-style.ts
+++ src/composables/use/use-style.ts
@@ -1,8 +1,9 @@
 import getStyleFromFile from '@/utils/style';
 import { onMounted, ref } from 'vue';
+import parse from 'style-to-object';
 
 export default function useStyle(cssFileName: string) {
-  const style = ref<any>({});
+  const style = ref<Record<string, ReturnType<typeof parse>>>({});
 
   onMounted(async () => {
     style.value = await getStyleFromFile(cssFileName);
